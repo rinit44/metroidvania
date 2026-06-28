e52d9fc9f9187e58fc33ab60881cdc05793294fc
rinit44 <rinit44@gmail.com>
2026-06-28 13:55:58 +0200

    feat: add platform collision detection

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,15 @@ int main() {
     InitWindow(screenWidth, screenHeight, "Elemics");
     SetTargetFPS(60);
 
-    // Position du joueur (rectangle)
     float playerX = 400;
     float playerY = 300;
     float velocityY = 0;
     float gravity = 0.5;
     bool isOnGround = false;
+    float platformX = 400;
+    float platformY = 450;
+    float platformWidth = 100;
+    float platformHeight = 40;
 
     // === LA BOUCLE DE JEU ===
     while (!WindowShouldClose()) {
@@ -29,6 +32,8 @@ int main() {
         // === GRAVITÉ ===
         velocityY = velocityY + gravity;
         playerY = playerY + velocityY;
+        float playerBottom = playerY + 60;   // les pieds
+        float playerRight = playerX + 40;    // le bord droit
 
         // === SOL ===
         if (playerY > 500) {
@@ -46,12 +51,22 @@ int main() {
         if (playerX > 760) {
             playerX = 760;
         }
+          // === COLLISION PLATEFORME ===
+        if (playerRight > platformX &&
+            playerX < platformX + platformWidth &&
+            playerBottom > platformY &&
+            velocityY > 0) {
+            playerY = platformY - 60;
+            velocityY = 0;
+            isOnGround = true;
+        }
 
         // === DESSIN ===
         BeginDrawing();
             ClearBackground(BLACK);
-            DrawRectangle(playerX, playerY, 40, 60, BLUE);
-            DrawRectangle(0, 560, 800, 40, GRAY);
+            DrawRectangle(playerX, playerY, 40, 60, BLUE); // joueur
+            DrawRectangle(0, 560, 800, 40, GRAY); // sol
+            DrawRectangle(platformX, platformY, platformWidth, platformHeight, GREEN); // plateforme
         EndDrawing();
 
     }
