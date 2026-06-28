5e41bd9029f806554f2b537edb415d6601956240
rinit44 <rinit44@gmail.com>
2026-06-28 11:36:58 +0200

    feat: Add player movement with WASD

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,24 @@ int main() {
     // Limite à 60 images par seconde
     SetTargetFPS(60);
 
+    // Position du joueur (rectangle)
+    float playerX = 400;
+    float playerY = 300;
+
     // === LA BOUCLE DE JEU ===
     // Tourne tant que le joueur ne ferme pas la fenêtre
     while (!WindowShouldClose()) {
+
+        // === INPUT : on lit les touches (WASD) ===
+        if (IsKeyDown(KEY_D)) playerX = playerX + 10;
+        if (IsKeyDown(KEY_A)) playerX = playerX - 10;
+        if (IsKeyDown(KEY_S)) playerY = playerY + 10;
+        if (IsKeyDown(KEY_W)) playerY = playerY - 10;
+
+        // === DESSIN ===
         BeginDrawing();
-            ClearBackground(BLACK); // fond noir
+            ClearBackground(BLACK);
+            DrawRectangle(playerX, playerY, 40, 60, BLUE);
         EndDrawing();
     }
 
